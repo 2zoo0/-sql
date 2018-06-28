115080093e74f6d4d9f6fc29d14946d6423f12ce
2zoo0 <40552904+2zoo0@users.noreply.github.com>
2018-06-28 09:18:22 +0900

    Update sql_day03.sql

--- sql_day03.sql
+++ sql_day03.sql
@@ -1,5 +1,14 @@
+--- 6) CASE
+
+/*
+    CLERK : 5%
+    SALESMAN : 4%
+    PRESIDENT : 3.7%
+    MANAGER : 3%
+    ANALYST : 1.5%
+*/
 -- 1. Simple CASE 구문으로 구해보자 : DECODE와 거의 유사, 동일 비교만 가능
-                                     괄호가 없고, 콤마 대신 키워드 WHEN, THEN, ELSE 등을 사용
+--                                   괄호가 없고, 콤마 대신 키워드 WHEN, THEN, ELSE 등을 사용
 SELECT e.EMPNO
      , e.ENAME
      , e.JOB
