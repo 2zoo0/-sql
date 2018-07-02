4efb6b9b1f23c78360e058716232d50bd6d7c6a8
2zoo0 <40552904+2zoo0@users.noreply.github.com>
2018-07-02 16:21:54 +0900

    Update lab05.sql

--- lab05.sql
+++ lab05.sql
@@ -57,3 +57,167 @@ TRUNCATE TABLE customer;
 
 -- Table CUSTOMER이(가) 잘렸습니다.
 
+----- 시퀀스ㅡ
+
+-- 실습 1)
+CREATE SEQUENCE seq_cust_userid
+MAXVALUE 99
+NOCYCLE
+;
+-- Sequence SEQ_CUST_USERID이(가) 생성되었습니다.
+
+-- 실습 2)
+SELECT S.sequence_name
+     , S.min_value
+     , S.max_value
+     , S.increment_by
+     , s.CYCLE_FLAG
+  FROM user_sequences s
+ WHERE S.sequence_name = 'SEQ_CUST_USERID'
+;
+/*
+SEQUENCE_NAME, MIN_VALUE, MAX_VALUE, INCREMENT_BY, CYCLE_FLAG
+----------------------------------------------------------------
+SEQ_CUST_USERID	1	99	1	N
+*/
+
+-- 실습 3)
+CREATE INDEX idx_cust_userid
+    ON customer (userid);
+-- 01408. 00000 -  "such column list already indexed"
+-- 실습 1) 에서 userid가 primary key 제약조건을 갖고 생성되어
+
+SELECT a.CONSTRAINT_NAME
+     , a.CONSTRAINT_TYPE
+     , a.SEARCH_CONDITION
+     , a.TABLE_NAME
+  FROM ALL_CONSTRAINTS a
+ WHERE a.TABLE_NAME = 'CUSTOMER';
+ /*
+SYS_C007001	C	"NAME" IS NOT NULL	CUSTOMER
+SYS_C007002	Primary_Key	            CUSTOMER
+ */
+
+-- 자동으로 SYS_C007002 인덱스가 등록됨
+-- 실습용이니 그냥 제약조건 없애버리고 하겠습니다.
+ALTER TABLE customer
+DROP CONSTRAINT SYS_C007002;
+
+CREATE INDEX idx_cust_userid
+    ON customer (userid);
+--Index IDX_CUST_USERID이(가) 생성되었습니다.
+
+-- 실습 4)
+DESC USER_INDEXES;
+
+/*
+이름                      널?       유형             
+----------------------- -------- -------------- 
+INDEX_NAME              NOT NULL VARCHAR2(30)   
+INDEX_TYPE                       VARCHAR2(27)   
+TABLE_OWNER             NOT NULL VARCHAR2(30)   
+TABLE_NAME              NOT NULL VARCHAR2(30)   
+TABLE_TYPE                       VARCHAR2(11)   
+UNIQUENESS                       VARCHAR2(9)    
+COMPRESSION                      VARCHAR2(8)    
+PREFIX_LENGTH                    NUMBER         
+TABLESPACE_NAME                  VARCHAR2(30)   
+INI_TRANS                        NUMBER         
+MAX_TRANS                        NUMBER         
+INITIAL_EXTENT                   NUMBER         
+NEXT_EXTENT                      NUMBER         
+MIN_EXTENTS                      NUMBER         
+MAX_EXTENTS                      NUMBER         
+PCT_INCREASE                     NUMBER         
+PCT_THRESHOLD                    NUMBER         
+INCLUDE_COLUMN                   NUMBER         
+FREELISTS                        NUMBER         
+FREELIST_GROUPS                  NUMBER         
+PCT_FREE                         NUMBER         
+LOGGING                          VARCHAR2(3)    
+BLEVEL                           NUMBER         
+LEAF_BLOCKS                      NUMBER         
+DISTINCT_KEYS                    NUMBER         
+AVG_LEAF_BLOCKS_PER_KEY          NUMBER         
+AVG_DATA_BLOCKS_PER_KEY          NUMBER         
+CLUSTERING_FACTOR                NUMBER         
+STATUS                           VARCHAR2(8)    
+NUM_ROWS                         NUMBER         
+SAMPLE_SIZE                      NUMBER         
+LAST_ANALYZED                    DATE           
+DEGREE                           VARCHAR2(40)   
+INSTANCES                        VARCHAR2(40)   
+PARTITIONED                      VARCHAR2(3)    
+TEMPORARY                        VARCHAR2(1)    
+GENERATED                        VARCHAR2(1)    
+SECONDARY                        VARCHAR2(1)    
+BUFFER_POOL                      VARCHAR2(7)    
+FLASH_CACHE                      VARCHAR2(7)    
+CELL_FLASH_CACHE                 VARCHAR2(7)    
+USER_STATS                       VARCHAR2(3)    
+DURATION                         VARCHAR2(15)   
+PCT_DIRECT_ACCESS                NUMBER         
+ITYP_OWNER                       VARCHAR2(30)   
+ITYP_NAME                        VARCHAR2(30)   
+PARAMETERS                       VARCHAR2(1000) 
+GLOBAL_STATS                     VARCHAR2(3)    
+DOMIDX_STATUS                    VARCHAR2(12)   
+DOMIDX_OPSTATUS                  VARCHAR2(6)    
+FUNCIDX_STATUS                   VARCHAR2(8)    
+JOIN_INDEX                       VARCHAR2(3)    
+IOT_REDUNDANT_PKEY_ELIM          VARCHAR2(3)    
+DROPPED                          VARCHAR2(3)    
+VISIBILITY                       VARCHAR2(9)    
+DOMIDX_MANAGEMENT                VARCHAR2(14)   
+SEGMENT_CREATED                  VARCHAR2(3)
+*/
+
+
+DESC USER_IND_COLUMNS;
+/*
+이름              널? 유형             
+--------------- -- -------------- 
+INDEX_NAME         VARCHAR2(30)   
+TABLE_NAME         VARCHAR2(30)   
+COLUMN_NAME        VARCHAR2(4000) 
+COLUMN_POSITION    NUMBER         
+COLUMN_LENGTH      NUMBER         
+CHAR_LENGTH        NUMBER         
+DESCEND            VARCHAR2(4)   
+*/
+
+-- 실습 5) 
+SELECT u.*
+  FROM USER_INDEXES u
+ WHERE U.INDEX_NAME = 'IDX_CUST_USERID' 
+;
+ /*
+ INDEX_NAME, INDEX_TYPE, TABLE_OWNER, TABLE_NAME, TABLE_TYPE, UNIQUENESS, COM ....
+ --------------------------------------------------------------------------------
+ IDX_CUST_USERID	NORMAL	SCOTT	CUSTOMER	TABLE	UNIQUE	DISABLED	....
+ 
+ */
+ 
+ -- 실습 6) 
+SELECT u.*
+  FROM USER_IND_COLUMNS u
+ WHERE U.INDEX_NAME = 'IDX_CUST_USERID' 
+;
+ 
+ /*
+ INDEX_NAME, TABLE_NAME, COLUMN_NAME, COLUMN_POSITION, COLUMN_LENGTH, CHAR_LENGTH, DESCEND
+ -----------------------------------------------------------------------------------------
+ IDX_CUST_USERID	CUSTOMER	USERID	1	4	4	ASC
+ */
+ 
+-- 실습 7) 
+DROP INDEX IDX_CUST_USERID;
+--Index IDX_CUST_USERID이(가) 삭제되었습니다.    
+
+-- 실습 8) 
+SELECT u.*
+  FROM USER_IND_COLUMNS u
+ WHERE U.INDEX_NAME = 'IDX_CUST_USERID' 
+;
+-- 'IDX_CUST_USERID' 인덱스가 없으므로 삭제된 것을 확인.
+
