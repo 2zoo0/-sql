fa2512fff4fdf8512760ce601822a9b541063733
2zoo0 <>
2018-07-06 13:37:18 +0900

    sp commit

--- SP_lap.sql
+++ SP_lap.sql
@@ -33,6 +33,11 @@ INCREMENT BY 1
 NOMAXVALUE
 NOCYCLE
 ;
+
+-- INSERT 문 테스트
+INSERT INTO CUSTOMER c(c.userid, c.name, c.address, c.phone)
+    VALUES ('C'||lpad(SEQ_CUST_USERID.nextval,3,0), '2zoo0', 'Daejeon', '01051396628'); 
+
 -- Sequence SEQ_CUST_USERID이(가) 생성되었습니다.
 
 SELECT SEQ_CUST_USERID.CURRVAL FROM dual;
@@ -41,9 +46,6 @@ SELECT SEQ_CUST_USERID.CURRVAL FROM dual;
 SELECT 'C'||lpad(SEQ_CUST_USERID.CURRVAL,3,0) as "lpad_Seq" from dual;
 
 
--- INSERT 문 테스트
-INSERT INTO CUSTOMER c(c.userid, c.name, c.address, c.phone)
-    VALUES ('C'||lpad(SEQ_CUST_USERID.nextval,3,0), '2zoo0', 'Daejeon', '01051396628'); 
 
 
 CREATE OR REPLACE PROCEDURE sp_insert_customer
@@ -59,11 +61,12 @@ BEGIN
     VALUES ('C' || lpad(SEQ_CUST_USERID.NEXTVAL,3,0), v_name, v_address, v_phone);
     
     v_msg := 'C'||lpad(SEQ_CUST_USERID.CURRVAL,3,0) || ', '|| v_name ;
+    
 END sp_insert_customer;
 /
 --Procedure SP_INSERT_CUSTOMER이(가) 컴파일되었습니다.
 
-VAR v_insert_customer_msg VARCHAR2(200)
+VAR v_insert_customer_msg VARCHAR2(100)
 
 EXECUTE sp_insert_customer('2zoo0', 'Daejeon', '01051396628', :v_insert_customer_msg)
 -- PL/SQL 프로시저가 성공적으로 완료되었습니다.
